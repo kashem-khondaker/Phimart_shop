6cca722e0999898e54fa2705ffdae942e4c2f761
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-03-16 04:54:00 +0600

    Cart and CartItem system implemented

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,24 @@
 from django.urls import path , include
 from product.views import ProductViewSet , CategoryViewSet , ReviewViewSet
+from order.views import CartViewSet , CartItemViewSet
 from rest_framework_nested import routers
 
 router =routers.DefaultRouter()
 router.register('products' , ProductViewSet , basename='products')
-router.register('categories' , CategoryViewSet)
+router.register('categories' , CategoryViewSet , basename='categories')
+router.register('carts' , CartViewSet , basename='carts')
 
 product_router = routers.NestedSimpleRouter(router, r'products', lookup='product')
 product_router.register(r'reviews', ReviewViewSet, basename='product-review')
+cart_router = routers.NestedDefaultRouter(router , r'carts' , lookup = 'cart')
+cart_router.register(r'items' , CartItemViewSet , basename='cart-item')
 
 # urlpatterns = router.urls
 
 urlpatterns = [
     path('' , include(router.urls)),
     path('' , include(product_router.urls)),
+    path('' , include(cart_router.urls))
 ]
 
 # urlpatterns = [
--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import path , include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
