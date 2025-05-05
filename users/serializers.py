eae54416a7e62ea5e64db4634bc5e461187b5274
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-05-05 19:38:06 +0600

    Product Image and is_staff status added

--- users/serializers.py
+++ users/serializers.py
@@ -11,5 +11,6 @@ class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         ref_name = 'CustomUser'
         model = get_user_model()
-        fields = ['id' , 'email' , 'first_name' , 'last_name' , 'address' , 'phone']
+        fields = ['id' , 'email' , 'first_name' , 'last_name' , 'address' , 'phone' , 'is_staff' ]
+        read_only_fields = ['is_staff' ]
 
